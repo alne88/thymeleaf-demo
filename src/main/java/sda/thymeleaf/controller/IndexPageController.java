2a10e9a25faaaa72e421a2098ac018cc7a442272
Alo <41338019+alne88@users.noreply.github.com>
2018-11-28 19:49:08 +0200

    Now has more stuff at webpage

--- src/main/java/sda/thymeleaf/controller/IndexPageController.java
+++ src/main/java/sda/thymeleaf/controller/IndexPageController.java
@@ -3,13 +3,27 @@ package sda.thymeleaf.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import sda.thymeleaf.dbo.Person;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class IndexPageController {
 
     @RequestMapping("/")
     public String mainPage(Model model) {
+        //this is like in JSP setAttribute
+
         model.addAttribute("greeting", "Hello fellow samaritan");
+        model.addAttribute("person", new Person("Alo", "Nelis", 30));
+
+        List<Person> students = new ArrayList<>();
+        students.add(new Person("Daniel", "Richards", 26));
+        students.add(new Person("Sander", "One", 15));
+        students.add(new Person("Oskar", "Two", 90));
+        model.addAttribute("students", students);
+
         return "index";
     }
 }
